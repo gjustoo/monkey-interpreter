c8704f6c1499b36680d2bd61f65071cc225b79a9
gjustoo <gabrijusto113@gmail.com>
2024-02-24 01:11:30 +0100

    Implemented boolean infix evaluations

--- src/monkey/evaluator/evaluator.go
+++ src/monkey/evaluator/evaluator.go
@@ -46,6 +46,10 @@ func evalInfixExpression(operator string, left, right object.Object) object.Obje
 	switch {
 	case left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ:
 		return evalIntegerInfixExpression(operator, left, right)
+	case operator == "==":
+		return nativeBoolToBoolObject(left == right)
+	case operator == "!=":
+		return nativeBoolToBoolObject(left != right)
 	default:
 		return NULL
 
